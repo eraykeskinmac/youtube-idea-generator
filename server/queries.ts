1646b4a71aa0f546b04294f8f0855b402376ffad
eraykeskinmac <eraykeskinmac@gmail.com>
2024-12-07 16:06:32 +0300

    feat: add functionality to retrieve videos with comments and user ideas

--- server/queries.ts
+++ server/queries.ts
@@ -1,9 +1,17 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
-import { Video, Videos, YouTubeChannels, YouTubeChannelType } from "./db/schema";
-import { eq } from "drizzle-orm";
+import {
+  Idea,
+  Ideas,
+  Video,
+  VideoComments,
+  Videos,
+  YouTubeChannels,
+  YouTubeChannelType,
+} from "./db/schema";
 import { db } from "./db/drizzle";
+import { eq, and, desc } from "drizzle-orm";
 
 export const getVideosForUser = async (): Promise<Video[]> => {
   const { userId } = await auth();
@@ -21,8 +29,55 @@ export const getChannelsForUser = async (): Promise<YouTubeChannelType[]> => {
   if (!userId) {
     throw new Error("User not authenticated");
   }
+
   return db
     .select()
     .from(YouTubeChannels)
     .where(eq(YouTubeChannels.userId, userId));
+};
+
+export const getVideoWithComments = async (
+  videoId: string
+): Promise<{
+  video: Video;
+  comments: (typeof VideoComments.$inferSelect)[];
+} | null> => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const videos = await db
+    .select()
+    .from(Videos)
+    .where(and(eq(Videos.id, videoId), eq(Videos.userId, userId)));
+
+  if (videos.length === 0) {
+    return null;
+  }
+
+  const video = videos[0];
+
+  const comments = await db
+    .select()
+    .from(VideoComments)
+    .where(eq(VideoComments.videoId, videoId))
+    .orderBy(VideoComments.publishedAt);
+
+  return { video, comments };
+};
+
+export const getIdeasForUser = async (): Promise<Idea[]> => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  return db
+    .select()
+    .from(Ideas)
+    .where(eq(Ideas.userId, userId))
+    .orderBy(desc(Ideas.createdAt));
 };
